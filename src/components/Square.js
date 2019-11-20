84c417cb8060a2193343e47a0b12ffbd99fd32ea
laurapauly <laurapauly@arcor.de>
2019-11-20 11:58:16 +0100

    add styling to js to delete css file

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background: #3b8277;
+  border: 1px solid #fff;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 50px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 50px;
+  outline: none;
+  color: #cc584d;
+`;
 
 // <Square value = X />
 export default function Square({ value, onClick }) {
-  return (
-    <button className="square" onClick={onClick}>
-      {value}
-    </button>
-  );
+  return <Button onClick={onClick}>{value}</Button>;
 }
 
 // <Square value = X />
---
+++ src/components/Status.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+const GameStatus = styled.div`
+  margin-bottom: 10px;
+`;
+
+export default GameStatus;
